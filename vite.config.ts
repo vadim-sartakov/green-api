ba35e6d5b7786a6a1b41ad6aac74f839c32a1784
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2026-09-23 12:35:31 +0500

    Base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     tsconfigPaths: true,
   },
   test: {
     environment: 'node',
   },
+  base: '/green-api/',
 });
